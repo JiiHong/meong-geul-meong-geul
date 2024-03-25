e7d6b684e20abc6d7b95fa8599919996444b6d34
JiiHong <youhae102@gmail.com>
2024-03-26 04:37:23 +0900

    [#1] Chore: tailwind config 세팅 - 반응형 스크린 Max-width breakpoints로 변경 - gray 팔레트 변경 - border-radius, box-shadow 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,35 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: { max: '639px' },
+      md: { max: '767px' },
+      lg: { max: '1023px' },
+      xl: { max: '1279px' },
+      '2xl': { max: '1535px' },
+    },
+    extend: {
+      colors: {
+        gray: {
+          50: '#f7f7f7',
+          100: '#eaeaea',
+          200: '#dddddd',
+          300: '#d3d3d3',
+          400: '#cacaca',
+          500: '#b0b0b0',
+          600: '#999999',
+          700: '#363939',
+          800: '#353535',
+          900: '#050505',
+        },
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      boxShadow: {
+        base: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+      },
+    },
   },
   plugins: [],
 };
