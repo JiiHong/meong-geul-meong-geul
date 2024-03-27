0d2557b3e08564237585cfe12fb97f31d3159dda
JiiHong <youhae102@gmail.com>
2024-03-27 19:18:48 +0900

    [#11] Design: div 컨테이너, header 모바일 화면 패딩 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${inter.className} bg-gray-100 py-5`}>
-        <div className="w-full max-w-5xl mx-auto p-8 bg-gray-50 rounded-4xl">
+        <div className="w-full max-w-5xl mx-auto p-8 bg-gray-50 rounded-4xl lg:p-4">
           <Header />
           {children}
         </div>
