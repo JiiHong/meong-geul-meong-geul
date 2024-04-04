5bf77adfc85ec985ba4deebe71d59c103a241b02
JiiHong <youhae102@gmail.com>
2024-04-04 19:40:37 +0900

    [#17] Design: UserDropdown 위치, 폰트 굵기 변경

--- src/components/Header/UserDropdown.tsx
+++ src/components/Header/UserDropdown.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function UserDropdown({ isActive }: Props) {
   return (
     <ul
-      className={`absolute top-[3.3rem] left-1/2 -translate-x-1/2 p-4 break-keep border bg-gray-50 shadow-lg rounded-lg after:absolute after:left-1/2 after:-top-[0.54rem] after:-translate-x-1/2 after:rotate-45 after:w-4 after:h-4 after:border-t after:border-l after:bg-gray-50 after:rounded-sm after:shadow-2xl [&_li]:px-4 [&_li]:py-1 [&_li]:text-sm [&_li]:text-gray-700 [&_li]:font-semibold [&_li]:rounded-lg
+      className={`absolute top-16 left-1/2 -translate-x-1/2 p-4 break-keep border bg-gray-50 shadow-lg rounded-lg after:absolute after:left-1/2 after:-top-[0.54rem] after:-translate-x-1/2 after:rotate-45 after:w-4 after:h-4 after:border-t after:border-l after:bg-gray-50 after:rounded-sm after:shadow-2xl [&_li]:px-4 [&_li]:py-1 [&_li]:text-sm [&_li]:text-gray-700 [&_li]:font-bold [&_li]:rounded-lg
     ${isActive ? 'flex flex-col items-center gap-4 ' : 'hidden'}`}
     >
       <li className="hover:bg-gray-400">마이페이지</li>
