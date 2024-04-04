7cf5e0c7a5e3d7ef1e5f60d54756e5cae17c5ae1
JiiHong <youhae102@gmail.com>
2024-04-04 18:37:51 +0900

    [#17] Feat: UserImage 누르면 Dropdown 나오게 구현

--- src/components/Header/UserImage.tsx
+++ src/components/Header/UserImage.tsx
@@ -1,22 +1,29 @@
 'use client';
 
-import Image from 'next/image';
 import { useUserContext } from '@/context/UserContext';
+import Image from 'next/image';
+import UserDropdown from './UserDropdown';
+import { useState } from 'react';
 
 export default function UserImage() {
   const { user } = useUserContext();
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => setIsActive((active) => !active);
 
   return (
-    <div>
+    <div className="relative">
       {user && (
         <Image
           src={user.photoURL}
           alt={user.displayName}
           width={40}
           height={40}
-          className="rounded-full"
+          className="rounded-full cursor-pointer"
+          onClick={handleClick}
         />
       )}
+      {isActive && <UserDropdown isActive={isActive} />}
     </div>
   );
 }
