0613fa9ae77af7515fbba68bf296b85fd20a3166
JiiHong <youhae102@gmail.com>
2024-04-06 00:11:15 +0900

    [#17] Feat: 로그아웃하면 세션쿠키 삭제하는 logout route handler 추가

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,14 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+  const options = {
+    name: 'session',
+    value: '',
+    maxAge: -1,
+  };
+
+  cookies().set(options);
+
+  return NextResponse.json({}, { status: 200 });
+}
