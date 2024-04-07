3afa5d34adeaff587f0f79ec5b6078a7fcec7c9a
JiiHong <youhae102@gmail.com>
2024-04-08 01:52:24 +0900

    [#30] Fix: tailwind config screens 순서 변경 - breakpoints를 max-width로 변경하면 크기 순서가 내림차순으로 되어있어야 함.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     screens: {
-      sm: { max: '639px' },
-      md: { max: '767px' },
-      lg: { max: '1023px' },
-      xl: { max: '1279px' },
       '2xl': { max: '1535px' },
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
     },
     extend: {
       colors: {
