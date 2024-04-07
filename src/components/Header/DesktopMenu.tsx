750e996aa4de898998a00427f75f47fc06b70fe6
JiiHong <102526510+JiiHong@users.noreply.github.com>
2024-04-07 17:39:08 +0900

    [#26] Design: 헤더 max-width md로 변경 (#27)

--- src/components/Header/DesktopMenu.tsx
+++ src/components/Header/DesktopMenu.tsx
@@ -2,7 +2,7 @@ import Menu from './Menu';
 
 export default function DesktopMenu() {
   return (
-    <div className="flex items-center gap-6 lg:hidden">
+    <div className="flex items-center gap-6 md:hidden">
       <Menu className={{ ul: 'flex items-center gap-4' }} />
     </div>
   );
