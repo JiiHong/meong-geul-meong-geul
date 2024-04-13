95d14c243fa81461473ed789347fb57628cfaf12
JiiHong <youhae102@gmail.com>
2024-04-14 01:08:53 +0900

    [#30] Fix: 모바일 메뉴 aside 창 펼쳤을 때 BoardCard가 위로 나오는 버그 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 left-0 flex justify-center items-center w-full h-10 px-12 py-8 shadow-base rounded-full bg-white lg:px-6 lg:py-7">
+    <header className="sticky top-0 left-0 flex justify-center items-center w-full h-10 px-12 py-8 shadow-base rounded-full bg-white z-10 lg:px-6 lg:py-7">
       <div className="flex justify-between items-center w-full max-w-5xl">
         <Link href="/" className="text-3xl font-bold">
           {/* TODO: 로고 변경 */}
