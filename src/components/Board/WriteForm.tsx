53d75ccf2d3e1a397e411cd81c44186848f94237
JiiHong <youhae102@gmail.com>
2024-04-17 20:20:21 +0900

    [#30] Feat: input file 커스텀

--- src/components/Board/WriteForm.tsx
+++ src/components/Board/WriteForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ChangeEvent, useState } from 'react';
+import { MdOutlineAddPhotoAlternate } from 'react-icons/md';
 import { Board } from '@/types/board';
 
 const DEFAULT_DATA = {
@@ -11,13 +12,21 @@ const DEFAULT_DATA = {
 export default function WriteForm() {
   const [board, setBoard] =
     useState<Pick<Board, 'title' | 'content'>>(DEFAULT_DATA);
+  const [fileName, setFileName] = useState('');
 
-  const handleChange = (
+  const handleTextChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     const name = e.target.name;
     const value = e.target.value;
     setBoard({ ...board, [name]: value });
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    const file = files && files[0];
+    const name = file ? file.name : '';
+    setFileName(name);
   };
 
   return (
@@ -29,7 +38,7 @@ export default function WriteForm() {
         maxLength={29}
         required
         placeholder="제목"
-        onChange={handleChange}
+        onChange={handleTextChange}
         className="px-4 py-2 text-3xl border-b outline-none md:px-2 md:py-1 md:text-2xl"
       />
 
@@ -39,15 +48,26 @@ export default function WriteForm() {
         value={board.content}
         required
         placeholder="내용"
-        onChange={handleChange}
+        onChange={handleTextChange}
         className="px-4 py-2 text-lg border outline-none rounded-lg md:px-2 md:py-1 md:text-base"
       />
-      <input
-        type="file"
-        name="file"
-        accept="image/*"
-        className="p-2 border rounded-lg md:text-xs"
-      />
+      <div className="flex items-center gap-2 border rounded-lg">
+        <label
+          htmlFor="file"
+          className="px-3 py-1 text-2xl text-white rounded-sm bg-gray-600 cursor-pointer"
+        >
+          <MdOutlineAddPhotoAlternate />
+        </label>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          accept="image/*"
+          className="hidden"
+          onChange={handleFileChange}
+        />
+        <p className="text-sm text-gray-700">{fileName}</p>
+      </div>
       <div className="self-end flex gap-1 text-lg [&_button]:px-6 [&_button]:py-2 [&_button]:rounded-lg md:[&_button]:px-4 md:[&_button]:py-1 md:[&_button]:text-base">
         <button className="border">취소</button>
         <button className="text-white border bg-amber-500">등록</button>
