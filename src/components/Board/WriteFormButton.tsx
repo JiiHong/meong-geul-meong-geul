5c8c445c65187f40110361e53bf6a73d668d536d
JiiHong <youhae102@gmail.com>
2024-04-18 23:17:23 +0900

    [#30] Feat: 취소 버튼 누르면 이전 페이지로

--- src/components/Board/WriteFormButton.tsx
+++ src/components/Board/WriteFormButton.tsx
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/navigation';
+
 export default function WriteFormButton() {
+  const router = useRouter();
+
+  const handleCancelClick = () => router.back();
+
   return (
     <div className="self-end flex gap-1 text-lg [&_button]:px-6 [&_button]:py-2 [&_button]:rounded-lg md:[&_button]:px-4 md:[&_button]:py-1 md:[&_button]:text-base">
-      <button className="border">취소</button>
+      <button className="border" onClick={handleCancelClick}>
+        취소
+      </button>
       <button className="text-white border bg-amber-500">등록</button>
     </div>
   );
