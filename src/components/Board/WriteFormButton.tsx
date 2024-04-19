f1d29bf690a19ead81d219f20e72df73b85fd02a
JiiHong <youhae102@gmail.com>
2024-04-19 18:20:35 +0900

    [#30] Fix: 글 쓰기 페이지에서 취소했을 때 submit 안 되게 수정

--- src/components/Board/WriteFormButton.tsx
+++ src/components/Board/WriteFormButton.tsx
@@ -7,7 +7,7 @@ export default function WriteFormButton() {
 
   return (
     <div className="self-end flex gap-1 text-lg [&_button]:px-6 [&_button]:py-2 [&_button]:rounded-lg md:[&_button]:px-4 md:[&_button]:py-1 md:[&_button]:text-base">
-      <button className="border" onClick={handleCancelClick}>
+      <button type="button" className="border" onClick={handleCancelClick}>
         취소
       </button>
       <button className="text-white border bg-amber-500">등록</button>
