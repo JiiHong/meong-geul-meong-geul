0b60b0147d484c982809c5ab59ddb57846d961fa
JiiHong <youhae102@gmail.com>
2024-04-22 05:23:58 +0900

    [#30] Feat: react-query 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import { UserContextProvider } from '@/context/UserContext';
+import ReactQueryProvider from '@/components/ReactQueryProviders';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,11 +23,13 @@ export default function RootLayout({
     <html lang="ko">
       <body className={`${inter.className} bg-gray-100 py-5`}>
         <div className="w-full max-w-5xl mx-auto p-8 bg-gray-50 rounded-4xl lg:p-4">
-          <UserContextProvider>
-            <Header />
-            {children}
-            {loginModal}
-          </UserContextProvider>
+          <ReactQueryProvider>
+            <UserContextProvider>
+              <Header />
+              {children}
+              {loginModal}
+            </UserContextProvider>
+          </ReactQueryProvider>
         </div>
       </body>
     </html>
