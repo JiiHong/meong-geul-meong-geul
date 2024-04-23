8dd2622977286f835b69b888d5a7095d963a859e
JiiHong <youhae102@gmail.com>
2024-04-23 23:24:09 +0900

    [#30] Feat: 게시판 hydrate 설정

--- src/app/board/free/page.tsx
+++ src/app/board/free/page.tsx
@@ -1,15 +1,29 @@
+import {
+  QueryClient,
+  dehydrate,
+  HydrationBoundary,
+} from '@tanstack/react-query';
 import { headers } from 'next/headers';
 import WriteButton from '@/components/Board/WriteButton';
+import { fetchPosts } from '@/service/firebase/firebase-firestore';
 import Posts from '@/components/Board/Posts';
 
 export default async function FreeBoardPage() {
   const headersList = headers();
   const path = headersList.get('x-pathname') || '';
   const category = path.split('/')[2];
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['board', category],
+    queryFn: () => fetchPosts(category),
+  });
 
   return (
     <section>
-      <Posts />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Posts />
+      </HydrationBoundary>
       <WriteButton />
     </section>
   );
