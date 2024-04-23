e83626f9196fbc0b663e9815052b01079639d4e2
JiiHong <youhae102@gmail.com>
2024-04-24 01:52:07 +0900

    [#30] Design: 글쓰기 버튼 hover 추가

--- src/components/Board/WriteButton.tsx
+++ src/components/Board/WriteButton.tsx
@@ -9,7 +9,7 @@ export default function WriteButton() {
   const category = path.split('/')[2];
 
   return (
-    <div className="fixed bottom-10 right-5 text-2xl text-white rounded-full bg-gray-800">
+    <div className="fixed bottom-10 right-5 text-2xl text-white rounded-full bg-gray-800 transition-all hover:scale-110 hover:brightness-125">
       <Link href={`/write/${category}`} className="block w-full h-full p-2">
         <PiPencilSimpleLine />
       </Link>
