d0a8ed7dad69b2d276e8adfb78251b9f7742ad2d
JiiHong <youhae102@gmail.com>
2024-04-28 01:53:30 +0900

    [#30] Refactor: 게시판 상세 페이지 컨텐츠 BoardDetailContent 컴포넌트로 분리

--- src/app/board/free/[id]/page.tsx
+++ src/app/board/free/[id]/page.tsx
@@ -1,50 +1,18 @@
 import { headers } from 'next/headers';
 import { fetchPost } from '@/service/firebase/firebase-firestore';
 import { BoardCategory } from '@/types/board';
-import UserImage from '@/components/UserImage';
-import { formateFullTime } from '@/utils/day';
-import Image from 'next/image';
+import BoardDetailContent from '@/components/Board/BoardDetailContent';
 
 export default async function FreeBoardDetailPage() {
   const headersList = headers();
   const path = (headersList.get('x-pathname') || '').split('/');
   const category = path[2] as BoardCategory;
   const id = path[3];
-  const {
-    title,
-    name,
-    content,
-    viewCount,
-    createdAt,
-    userImage,
-    contentImage,
-  } = await fetchPost(category, id);
+  const post = await fetchPost(category, id);
 
   return (
     <main className="pt-8">
-      <section className="flex flex-col items-center p-8 rounded-3xl bg-white">
-        <div className="w-4/5">
-          <h1 className="mb-4 text-4xl font-semibold">{title}</h1>
-          <div className="flex items-center gap-4 pb-3 border-b">
-            <UserImage title={name} userImage={userImage} size={40} />
-            <div className="flex flex-col -space-y-2">
-              <span className="text-lg">{name}</span>
-              <div className="space-x-4">
-                <span className="text-sm text-gray-400">
-                  {formateFullTime(createdAt)}
-                </span>
-                <span className="text-sm text-gray-400">
-                  조회수 {viewCount}
-                </span>
-              </div>
-            </div>
-          </div>
-          <p className="py-6 text-xl break-words">{content}</p>
-          {contentImage && (
-            <Image src={contentImage} alt={title} width={450} height={450} />
-          )}
-        </div>
-      </section>
+      <BoardDetailContent post={post} />
     </main>
   );
 }
---
+++ src/components/Board/BoardDetailContent.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import { Board } from '@/types/board';
+import UserImage from '../UserImage';
+import { formateFullTime } from '@/utils/day';
+
+type Props = {
+  post: Board;
+};
+
+export default function BoardDetailContent({ post }: Props) {
+  const {
+    title,
+    name,
+    content,
+    viewCount,
+    createdAt,
+    userImage,
+    contentImage,
+  } = post;
+
+  return (
+    <section className="flex flex-col items-center p-8 rounded-3xl bg-white">
+      <div className="w-4/5">
+        <h1 className="mb-4 text-4xl font-semibold">{title}</h1>
+        <div className="flex items-center gap-4 pb-3 border-b">
+          <UserImage title={name} userImage={userImage} size={40} />
+          <div className="flex flex-col -space-y-2">
+            <span className="text-lg">{name}</span>
+            <div className="space-x-4">
+              <span className="text-sm text-gray-400">
+                {formateFullTime(createdAt)}
+              </span>
+              <span className="text-sm text-gray-400">조회수 {viewCount}</span>
+            </div>
+          </div>
+        </div>
+        <p className="py-6 text-xl break-words">{content}</p>
+        {contentImage && (
+          <Image
+            src={contentImage}
+            alt={title}
+            width={450}
+            height={450}
+            priority
+          />
+        )}
+      </div>
+    </section>
+  );
+}
