9c1c68d7069515f74f63e4ac6e748c04177dfd38
JiiHong <youhae102@gmail.com>
2024-04-27 23:41:24 +0900

    [#30] Fix: Skipping auto-scroll behavior due to `position: sticky` or `position: fixed` on element 경고 해결

--- src/components/Board/WriteButton.tsx
+++ src/components/Board/WriteButton.tsx
@@ -10,7 +10,11 @@ export default function WriteButton() {
 
   return (
     <div className="fixed bottom-10 right-5 text-2xl text-white rounded-full bg-gray-800 transition-all hover:scale-110 hover:brightness-125">
-      <Link href={`/write/${category}`} className="block w-full h-full p-2">
+      <Link
+        href={`/write/${category}`}
+        scroll={false}
+        className="block w-full h-full p-2"
+      >
         <PiPencilSimpleLine />
       </Link>
     </div>
