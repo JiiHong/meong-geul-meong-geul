3e75b4ad61efa0cabd81f4448869ecaf8bfec614
JiiHong <youhae102@gmail.com>
2024-05-03 04:20:54 +0900

    [#30] Feat: 댓글 창 hydrate 설정

--- src/components/Board/BoardDetailComments.tsx
+++ src/components/Board/BoardDetailComments.tsx
@@ -1,22 +1,28 @@
-'use client';
-
 import { Board, BoardCategory } from '@/types/board';
-import useComments from '@/hooks/useComments';
 import IconComment from '@/components/ui/IconComment';
 import IconHeart from '@/components/ui/IconHeart';
 import CommentForm from './CommentForm';
-import Comment from './Comment';
+import Comments from './Comments';
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query';
+import { fetchComments } from '@/service/firebase/firebase-firestore';
 
 type Props = {
   post: Board;
   category: BoardCategory;
 };
 
-export default function BoardDetailComments({ post, category }: Props) {
+export default async function BoardDetailComments({ post, category }: Props) {
   const { id, likeCount, commentCount } = post;
-  const {
-    commentQuery: { data: comments },
-  } = useComments(id, category);
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['comment', id],
+    queryFn: () => fetchComments(id, category),
+  });
 
   return (
     <section className="flex flex-col gap-4 p-8 mt-8 rounded-3xl bg-white">
@@ -26,17 +32,10 @@ export default function BoardDetailComments({ post, category }: Props) {
         <IconComment />
         <span>{commentCount}</span>
       </div>
-      <CommentForm postId={id} category={category} />
-      <ul>
-        {comments && (
-          <Comment
-            postId={id}
-            comments={comments}
-            category={category}
-            replyId={null}
-          />
-        )}
-      </ul>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <CommentForm postId={id} category={category} />
+        <Comments postId={id} category={category} />
+      </HydrationBoundary>
     </section>
   );
 }
---
+++ src/components/Board/Comments.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import useComments from '@/hooks/useComments';
+import Comment from './Comment';
+import { BoardCategory } from '@/types/board';
+
+type Props = {
+  postId: string;
+  category: BoardCategory;
+};
+
+export default function Comments({ postId, category }: Props) {
+  const {
+    commentQuery: { data: comments },
+  } = useComments(postId, category);
+
+  return (
+    <ul>
+      {comments && (
+        <Comment
+          postId={postId}
+          comments={comments}
+          category={category}
+          replyId={null}
+        />
+      )}
+    </ul>
+  );
+}
