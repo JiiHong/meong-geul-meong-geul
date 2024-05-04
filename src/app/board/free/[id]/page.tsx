3a85e0eaa4da02c8fdede1acf5af9e2ecff1e3f0
JiiHong <youhae102@gmail.com>
2024-05-04 21:52:30 +0900

    [#30] Fix: postQuery -> fetchQuery로 변경

--- src/app/board/free/[id]/page.tsx
+++ src/app/board/free/[id]/page.tsx
@@ -1,8 +1,13 @@
 import { headers } from 'next/headers';
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query';
 import { BoardCategory } from '@/types/board';
 import BoardDetailContent from '@/components/Board/BoardDetailContent';
 import BoardDetailComments from '@/components/Board/BoardDetailComments';
-import usePost from '@/hooks/usePost';
+import { fetchPost } from '@/service/firebase/firebase-firestore';
 
 export default async function FreeBoardDetailPage() {
   const headersList = headers();
@@ -10,15 +15,20 @@ export default async function FreeBoardDetailPage() {
   const category = path[2] as BoardCategory;
   const id = path[3];
 
-  const {
-    postQuery: { data: post },
-  } = usePost(category, id);
+  const queryClient = new QueryClient();
+
+  const post = await queryClient.fetchQuery({
+    queryKey: ['board', category, id],
+    queryFn: () => fetchPost(category, id),
+  });
 
   return (
     <main className="pt-8">
       {post && (
         <>
-          <BoardDetailContent post={post} />
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            <BoardDetailContent post={post} />
+          </HydrationBoundary>
           <BoardDetailComments post={post} category={category} />
         </>
       )}
