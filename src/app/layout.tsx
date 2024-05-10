2e4ad2ab6b9c733baa9e4b4c3c660b7143734326
JiiHong <youhae102@gmail.com>
2024-05-10 20:41:41 +0900

    [#30] Design: body 화면 사이즈마다 패딩 다르게 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${inter.className} bg-gray-100 py-5`}>
+      <body className={`${inter.className} p-5 md:px-0 bg-gray-100`}>
         <div className="w-full max-w-5xl mx-auto p-8 bg-gray-50 rounded-4xl lg:p-4">
           <ReactQueryProvider>
             <UserContextProvider>
