74c3c16a9f2698a1788b31a118ce272f618feb2f
JiiHong <youhae102@gmail.com>
2024-05-12 23:12:56 +0900

    [#30] Design: 글 쓰기, 취소 버튼 hover 추가

--- src/components/Board/WriteFormButton.tsx
+++ src/components/Board/WriteFormButton.tsx
@@ -7,10 +7,16 @@ export default function WriteFormButton() {
 
   return (
     <div className="self-end flex gap-1 text-lg [&_button]:px-6 [&_button]:py-2 [&_button]:rounded-lg md:[&_button]:px-4 md:[&_button]:py-1 md:[&_button]:text-base">
-      <button type="button" className="border" onClick={handleCancelClick}>
+      <button
+        type="button"
+        className="border hover:brightness-90"
+        onClick={handleCancelClick}
+      >
         취소
       </button>
-      <button className="text-white border bg-amber-500">등록</button>
+      <button className="text-white border bg-amber-500 hover:brightness-110">
+        등록
+      </button>
     </div>
   );
 }
