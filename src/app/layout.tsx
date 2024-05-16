74b7b2a962ad0466453e88eaf13d8bdbc91d46f9
JiiHong <youhae102@gmail.com>
2024-05-17 02:24:56 +0900

    [#33] Feat: next-auth Provider Context 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import { UserContextProvider } from '@/context/UserContext';
+import NextAuthContext from '@/context/NextAuthContext';
 import ReactQueryProvider from '@/components/ReactQueryProviders';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -24,11 +25,13 @@ export default function RootLayout({
       <body className={`${inter.className} p-5 md:px-0 bg-gray-100`}>
         <div className="w-full max-w-5xl mx-auto p-8 bg-gray-50 rounded-4xl lg:p-4">
           <ReactQueryProvider>
-            <UserContextProvider>
-              <Header />
-              {children}
-              {loginModal}
-            </UserContextProvider>
+            <NextAuthContext>
+              <UserContextProvider>
+                <Header />
+                {children}
+                {loginModal}
+              </UserContextProvider>
+            </NextAuthContext>
           </ReactQueryProvider>
         </div>
       </body>
