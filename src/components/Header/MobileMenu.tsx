fa5423cf4bed5b08eb9e0c27691675e0ae045d24
JiiHong <youhae102@gmail.com>
2024-05-18 03:28:06 +0900

    [#33] Design: z-index 추가

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -10,7 +10,7 @@ export default function MobileMenu() {
   const handleClick = () => setIsOpenMenu((prev) => !prev);
 
   return (
-    <div className="hidden md:flex">
+    <div className="hidden md:flex z-20">
       <button
         className={`text-3xl after:fixed after:top-0 after:left-0 after:w-full after:h-full after:bg-black after:transition-all after:duration-500 ${isOpenMenu ? 'after:visible after:opacity-30' : 'after:invisible after:opacity-0'}`}
         onClick={handleClick}
