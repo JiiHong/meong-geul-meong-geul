ec1b9a87505e9ab05dcd8a56f34f1894723b721b
JiiHong <youhae102@gmail.com>
2024-05-18 03:28:44 +0900

    [#33] Fix: UserProfileImage 컴포넌트에 userContext 관련 코드 삭제

--- src/components/Header/UserProfileImage.tsx
+++ src/components/Header/UserProfileImage.tsx
@@ -1,30 +1,26 @@
 'use client';
 
-import { useUserContext } from '@/context/UserContext';
 import Image from 'next/image';
 import UserDropdown from './UserDropdown';
 import { useState } from 'react';
 import { defaultUserImage } from '@/constants/image';
 
-export default function UserProfileImage() {
-  const { user } = useUserContext();
+export default function UserLoginImage() {
   const [isActive, setIsActive] = useState(false);
 
   const handleClick = () => setIsActive((active) => !active);
 
   return (
-    <div className="relative p-[0.125rem] bg-pofile-border rounded-full">
-      {user && (
-        <Image
-          src={user.profileImage ?? defaultUserImage}
-          alt={user.name}
-          width={45}
-          height={45}
-          className="rounded-full cursor-pointer p-[0.15rem] bg-white"
-          onClick={handleClick}
-        />
-      )}
+    <button className="relative p-[0.125rem] bg-pofile-border rounded-full">
+      <Image
+        src={defaultUserImage}
+        alt={'user'}
+        width={45}
+        height={45}
+        className="rounded-full cursor-pointer p-[0.15rem] bg-white"
+        onClick={handleClick}
+      />
       {isActive && <UserDropdown isActive={isActive} />}
-    </div>
+    </button>
   );
 }
