ec5b87d3c5cb56866ef23d2600038c180c7e58d2
JiiHong <youhae102@gmail.com>
2024-05-19 01:52:01 +0900

    [#35] Refactor: middleware 필요 없는 코드 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const session = request.cookies.get('session');
   const pathname = request.nextUrl.pathname;
-
-  if (
-    session &&
-    (pathname.startsWith('/login') || pathname.startsWith('/signup'))
-  ) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  if (!session && pathname.startsWith('/write')) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
 
   if (pathname.startsWith('/board')) {
     const requestHeaders = new Headers(request.headers);
@@ -26,12 +14,6 @@ export async function middleware(request: NextRequest) {
       },
     });
   }
-
-  // const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth/login`, {
-  //   headers: {
-  //     Cookie: `session=${session?.value}`,
-  //   },
-  // });
 }
 
 export const config = {
