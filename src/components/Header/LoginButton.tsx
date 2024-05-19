50a79f45bf2e91b7c205573009951c53538b401a
JiiHong <youhae102@gmail.com>
2024-05-20 03:00:33 +0900

    [#37] Refactor: LoginButton 컴포넌트 코드 수정

--- src/components/Header/LoginButton.tsx
+++ src/components/Header/LoginButton.tsx
@@ -1,21 +1,20 @@
 'use client';
 
-import { useState } from 'react';
+import { useModalContext } from '@/context/ModalContext';
 import LoginModal from '../AuthModal/LoingModal';
 
 export default function LoginButton() {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => setIsOpen((prev) => !prev);
+  const { isOpen, toggleModal } = useModalContext();
 
   return (
     <>
       <button
         className="px-4 py-2 text-base text-white font-bold rounded-3xl bg-gray-800 transition-all hover:-translate-y-1 hover:bg-gray-900 hover:brightness-120"
-        onClick={handleClick}
+        onClick={toggleModal}
       >
         로그인
       </button>
-      {isOpen && <LoginModal onClick={handleClick} />}
+      {isOpen && <LoginModal onClick={toggleModal} />}
     </>
   );
 }
