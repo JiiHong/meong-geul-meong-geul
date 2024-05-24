f0e64e8e4e6ea1e17ea6694ea2fa097c67e01e71
JiiHong <youhae102@gmail.com>
2024-05-24 22:27:01 +0900

    [#41] Feat: Loader 변경

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,5 +1,5 @@
-import { MoonLoader } from 'react-spinners';
+import { SyncLoader } from 'react-spinners';
 
 export default function Loader() {
-  return <MoonLoader color="#36d7b7" size={20} />;
+  return <SyncLoader color="#36d7b7" size={5} />;
 }
