6e8a80e51d803156da57376f807f8afea6dad211
JiiHong <youhae102@gmail.com>
2024-05-26 16:33:04 +0900

    [#47] Feat: 메인페이지 게시판 최근 글 목록 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import BoardCard from '@/components/main/boardCard/BoardCard';
 import HomeCarousel from '@/components/main/carousel/HomeCarousel';
 
 export default function Home() {
@@ -7,6 +8,23 @@ export default function Home() {
       <section className="space-y-3 text-center">
         <p className="text-4xl font-bold">멍글멍글</p>
         <p className="text-gray-600">함께하는 나의 반려견, 우리의 이야기</p>
+      </section>
+      <section className="flex md:flex-col flex-row gap-8 rounded-2xl ">
+        <BoardCard
+          category="info"
+          title="정보게시판"
+          calssName="bg-[#ECECEC]"
+        />
+        <BoardCard
+          category="question"
+          title="질문게시판"
+          calssName="bg-[#F7F3FA]"
+        />
+        <BoardCard
+          category="free"
+          title="자유게시판"
+          calssName="bg-[#CDDBEB4D]"
+        />
       </section>
     </main>
   );
