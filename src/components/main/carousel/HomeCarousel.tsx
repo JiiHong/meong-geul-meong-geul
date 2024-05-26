9e7feef04d44c8b93525455c3eedf5abc8537f51
JiiHong <youhae102@gmail.com>
2024-05-27 01:56:38 +0900

    [#64] Refactor: console.log 삭제

--- src/components/main/carousel/HomeCarousel.tsx
+++ src/components/main/carousel/HomeCarousel.tsx
@@ -15,7 +15,6 @@ export default function HomeCarousel() {
   parser.setUA(userAgent);
   const result = parser.getResult();
   const deviceType = (result.device && result.device.type) || 'desktop';
-  console.log(result);
 
   return (
     <MultiCarousel deviceType={deviceType}>
