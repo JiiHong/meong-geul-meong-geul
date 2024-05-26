8e367970702818709abe9b052c0ec5c05cf183b3
JiiHong <youhae102@gmail.com>
2024-05-26 16:29:47 +0900

    [#47] Feat: 메인 중간 텍스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main className="space-y-12">
       <HomeCarousel />
+      <section className="space-y-3 text-center">
+        <p className="text-4xl font-bold">멍글멍글</p>
+        <p className="text-gray-600">함께하는 나의 반려견, 우리의 이야기</p>
+      </section>
     </main>
   );
 }
