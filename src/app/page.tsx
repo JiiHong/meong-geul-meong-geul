ce93facfb525a9ce38fd1142cce71f379be536e7
JiiHong <youhae102@gmail.com>
2024-05-26 18:26:48 +0900

    [#47] Feat: 메인페이지 이미지 그리드 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import MainGridContainer from '@/components/main/MainGridContainer';
 import BoardCard from '@/components/main/boardCard/BoardCard';
 import HomeCarousel from '@/components/main/carousel/HomeCarousel';
 
 export default function Home() {
   return (
-    <main className="space-y-12">
+    <main className="sm:space-y-8 md:space-y-10 space-y-12">
       <HomeCarousel />
       <section className="space-y-3 text-center">
-        <p className="text-4xl font-bold">멍글멍글</p>
-        <p className="text-gray-600">함께하는 나의 반려견, 우리의 이야기</p>
+        <p className="sm:text-2xl md:text-3xl text-4xl font-bold">멍글멍글</p>
+        <p className="sm:text-xs md:text-sm text-gray-600">
+          함께하는 나의 반려견, 우리의 이야기
+        </p>
       </section>
       <section className="flex md:flex-col flex-row gap-8 rounded-2xl ">
         <BoardCard
@@ -27,14 +30,17 @@ export default function Home() {
         />
       </section>
       <section className="space-y-2 text-center">
-        <p className="text-lg text-gray-600">
+        <p className="sm:text-sm md:text-base text-lg text-gray-600">
           <span className="text-amber-500">함께하는 나의 반려견</span>, 우리의
           이야기
         </p>
-        <p className="text-lg text-gray-600">
+        <p className="sm:text-sm md:text-base text-lg text-gray-600">
           새로운 모험과 소중한 순간들을
           <span className="text-amber-500"> 함께 나누는 곳</span>
         </p>
+      </section>
+      <section>
+        <MainGridContainer />
       </section>
     </main>
   );
---
+++ src/components/main/MainGridContainer.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import gamza from '../../../public/HomeGridImage/gamza.jpeg';
+import haru from '../../../public/HomeGridImage/haru.jpeg';
+import made1 from '../../../public/HomeGridImage/made1.jpeg';
+import som from '../../../public/HomeGridImage/som.jpeg';
+import made2 from '../../../public/HomeGridImage/made2.jpeg';
+import gguriLucky from '../../../public/HomeGridImage/gguriLucky.jpeg';
+
+export default function MainGridContainer() {
+  return (
+    <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-6 gap-2 h-72 [&_div]:relative [&_div]:shadow-base [&_div]:rounded-lg [&_div]:overflow-hidden [&_div]:transition-all">
+      <div className="sm:hidden row-span-2 col-span-2 hover:scale-105">
+        <Image src={gamza} alt="감자" fill className="object-cover"></Image>
+      </div>
+      <div className="col-span-2 hover:scale-105">
+        <Image src={haru} alt="하루" fill className="object-cover"></Image>
+      </div>
+      <div className="hover:scale-105">
+        <Image src={made1} alt="그림1" fill className="object-cover"></Image>
+      </div>
+      <div className="sm:hidden md:hidden row-span-2 hover:scale-105">
+        <Image src={som} alt="솜이" fill className="object-cover"></Image>
+      </div>
+      <div className="hover:scale-105">
+        <Image src={made2} alt="그림2" fill className="object-cover"></Image>
+      </div>
+      <div className="col-span-2 hover:scale-105">
+        <Image
+          src={gguriLucky}
+          alt="꾸리, 럭키"
+          fill
+          className="object-cover"
+        ></Image>
+      </div>
+    </div>
+  );
+}
