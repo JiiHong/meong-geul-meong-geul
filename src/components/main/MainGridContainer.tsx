24d3c7fd87a900752706454a5671951ae9fdbe61
JiiHong <youhae102@gmail.com>
2024-05-27 01:39:22 +0900

    [#60] Feat: MainGridContainer Image 태그 sizes 속성 설정

--- src/components/main/MainGridContainer.tsx
+++ src/components/main/MainGridContainer.tsx
@@ -10,25 +10,56 @@ export default function MainGridContainer() {
   return (
     <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-6 gap-2 h-72 [&_div]:relative [&_div]:shadow-base [&_div]:rounded-lg [&_div]:overflow-hidden [&_div]:transition-all">
       <div className="sm:hidden row-span-2 col-span-2 hover:scale-105">
-        <Image src={gamza} alt="감자" fill className="object-cover"></Image>
+        <Image
+          src={gamza}
+          alt="감자"
+          fill
+          sizes="(max-width: 639px) 0vw, 30vw"
+          className="object-cover"
+        ></Image>
       </div>
       <div className="col-span-2 hover:scale-105">
-        <Image src={haru} alt="하루" fill className="object-cover"></Image>
+        <Image
+          src={haru}
+          alt="하루"
+          fill
+          sizes="(max-width: 639px) 70vw, 30vw"
+          className="object-cover"
+        ></Image>
       </div>
       <div className="hover:scale-105">
-        <Image src={made1} alt="그림1" fill className="object-cover"></Image>
+        <Image
+          src={made1}
+          alt="그림1"
+          fill
+          sizes="(max-width: 639px) 30vw, (max-width: 767px) 15vw, 15vw"
+          className="object-cover"
+        ></Image>
       </div>
       <div className="sm:hidden md:hidden row-span-2 hover:scale-105">
-        <Image src={som} alt="솜이" fill className="object-cover"></Image>
+        <Image
+          src={som}
+          alt="솜이"
+          fill
+          sizes="(max-width: 767px) 0vw, 15vw"
+          className="object-cover"
+        ></Image>
       </div>
       <div className="hover:scale-105">
-        <Image src={made2} alt="그림2" fill className="object-cover"></Image>
+        <Image
+          src={made2}
+          alt="그림2"
+          fill
+          sizes="(max-width: 639px) 30vw, 15vw"
+          className="object-cover"
+        ></Image>
       </div>
       <div className="col-span-2 hover:scale-105">
         <Image
           src={gguriLucky}
           alt="꾸리, 럭키"
           fill
+          sizes="(max-width: 639px) 60vw, (max-width: 767px) 30vw, 30vw"
           className="object-cover"
         ></Image>
       </div>
