3d72f44570c4d5cf366fa98f3b8849f242b511d5
JiiHong <102526510+JiiHong@users.noreply.github.com>
2024-05-28 02:00:20 +0900

    [#79] Fix: LoginModal 컴포넌트 위치 LoginButton 컴포넌트 안에서 Menu 컴포넌트로 이동 (#80)

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -3,9 +3,11 @@
 import { Session } from 'next-auth';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useModalContext } from '@/context/ModalContext';
 import { navbarList } from './Navbar';
 import UserProfileImage from './UserProfileImage';
 import LoginButton from './LoginButton';
+import LoginModal from '../AuthModal/LoingModal';
 
 type Props = {
   session: Session | null;
@@ -15,30 +17,34 @@ type Props = {
 
 export default function Menu({ session, onClick, className }: Props) {
   const pathname = usePathname();
+  const { loginOpen, toggleLoginOpen } = useModalContext();
 
   return (
-    <ul className={className.ul}>
-      {navbarList.map(({ title, path }) => (
-        <li
-          key={title}
-          className={`text-lg font-bold transition-all hover:-translate-y-1 hover:text-gray-700 ${className.li} ${path === pathname ? 'text-gray-700' : 'text-gray-400'}`}
-        >
-          <Link href={path} className="px-4 py-3" onClick={onClick}>
-            {title}
-          </Link>
+    <>
+      <ul className={className.ul}>
+        {navbarList.map(({ title, path }) => (
+          <li
+            key={title}
+            className={`text-lg font-bold transition-all hover:-translate-y-1 hover:text-gray-700 ${className.li} ${path === pathname ? 'text-gray-700' : 'text-gray-400'}`}
+          >
+            <Link href={path} className="px-4 py-3" onClick={onClick}>
+              {title}
+            </Link>
+          </li>
+        ))}
+        <li className="md:self-center">
+          {session ? (
+            <UserProfileImage
+              session={session}
+              name={session.user.name}
+              image={session.user.profileImage}
+            />
+          ) : (
+            <LoginButton />
+          )}
         </li>
-      ))}
-      <li className="md:self-center">
-        {session ? (
-          <UserProfileImage
-            session={session}
-            name={session.user.name}
-            image={session.user.profileImage}
-          />
-        ) : (
-          <LoginButton />
-        )}
-      </li>
-    </ul>
+      </ul>
+      {loginOpen && <LoginModal onClick={toggleLoginOpen} />}
+    </>
   );
 }
