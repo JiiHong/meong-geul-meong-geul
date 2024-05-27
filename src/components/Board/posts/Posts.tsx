5b5ec8d2624762e4d0aa23d3f9c992089582746f
JiiHong <youhae102@gmail.com>
2024-05-28 03:37:47 +0900

    [#83] Design: 게시글 목록 반응형 grid 수정

--- src/components/Board/posts/Posts.tsx
+++ src/components/Board/posts/Posts.tsx
@@ -21,7 +21,7 @@ export default function Posts({ category, page }: Props) {
     <>
       {posts && (
         <>
-          <ul className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 py-8">
+          <ul className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-cols-4 gap-6 py-8 :">
             {posts
               .slice(startIndex, startIndex + itemCountPerPage)
               .map((post) => (
