d1afa4666cbfd01a329339e3a5f2148126bf5586
JiiHong <youhae102@gmail.com>
2024-05-28 01:58:43 +0900

    [#79] Fix: LoginModal 컴포넌트 위치 LoginButton 컴포넌트 안에서 Menu 컴포넌트로 이동

--- src/components/Header/LoginButton.tsx
+++ src/components/Header/LoginButton.tsx
@@ -1,20 +1,16 @@
 'use client';
 
 import { useModalContext } from '@/context/ModalContext';
-import LoginModal from '../AuthModal/LoingModal';
 
 export default function LoginButton() {
-  const { loginOpen, toggleLoginOpen } = useModalContext();
+  const { toggleLoginOpen } = useModalContext();
 
   return (
-    <>
-      <button
-        className="px-4 py-2 text-base text-white font-bold rounded-3xl bg-gray-800 transition-all hover:-translate-y-1 hover:bg-gray-900 hover:brightness-120"
-        onClick={toggleLoginOpen}
-      >
-        로그인
-      </button>
-      {loginOpen && <LoginModal onClick={toggleLoginOpen} />}
-    </>
+    <button
+      className="px-4 py-2 text-base text-white font-bold rounded-3xl bg-gray-800 transition-all hover:-translate-y-1 hover:bg-gray-900 hover:brightness-120"
+      onClick={toggleLoginOpen}
+    >
+      로그인
+    </button>
   );
 }
