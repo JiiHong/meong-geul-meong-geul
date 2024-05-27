196103edf46bc01dfc770175136e7811d1c391c3
JiiHong <youhae102@gmail.com>
2024-05-27 18:44:27 +0900

    [#71] Feat: next.config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
