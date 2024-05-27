bf9e78d4e5d15ffc561ea0c009439fc6fa138a3a
JiiHong <youhae102@gmail.com>
2024-05-28 00:15:50 +0900

    [#75] Feat: 글 등록할 때 버튼에 Loader 추가

--- src/components/Board/write/WriteFormButton.tsx
+++ src/components/Board/write/WriteFormButton.tsx
@@ -1,3 +1,4 @@
+import Loader from '@/components/ui/Loader';
 import { useRouter } from 'next/navigation';
 
 type Props = { disabled: boolean };
@@ -11,6 +12,7 @@ export default function WriteFormButton({ disabled }: Props) {
     <div className="self-end flex gap-1 text-lg [&_button]:px-6 [&_button]:py-2 [&_button]:rounded-lg md:[&_button]:px-4 md:[&_button]:py-1 md:[&_button]:text-base">
       <button
         type="button"
+        disabled={disabled}
         className="border hover:brightness-90"
         onClick={handleCancelClick}
       >
@@ -20,7 +22,7 @@ export default function WriteFormButton({ disabled }: Props) {
         disabled={disabled}
         className="text-white border bg-amber-500 hover:brightness-110"
       >
-        등록
+        {disabled ? <Loader /> : '등록'}
       </button>
     </div>
   );
