ddfb6cec72dcec14bb3547d4107f3e40d78b24f4
JiiHong <youhae102@gmail.com>
2024-06-06 23:33:51 +0900

    [#117] DesktopMenu 컴포넌트 desktop 폴더로 이동

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/next-auth/options';
-import DesktopMenu from './DesktopMenu';
+import DesktopMenu from './desktop/DesktopMenu';
 import MobileMenu from './MobileMenu';
 
 type NavbarList = {
