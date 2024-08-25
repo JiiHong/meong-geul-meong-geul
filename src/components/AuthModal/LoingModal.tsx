130904f7efb731322ea8a4e729b46eb1c8f1f4d8
Jihong Min <102526510+JiiHong@users.noreply.github.com>
2024-08-25 21:28:06 +0900

    [Fix] 회원가입 페이지에서 모달 바깥 영역 클릭하면 로그인 모달 나오는 버그 수정 (#128)

--- src/components/AuthModal/LoingModal.tsx
+++ src/components/AuthModal/LoingModal.tsx
@@ -1,11 +1,14 @@
 import GoogleLoginButton from '@/components/AuthModal/GoogleLoginButton';
 import Modal from './Modal';
 import ModalPortal from './ModalPortal';
+import { useModalContext } from '@/context/ModalContext';
 
 export default function LoginModal() {
+  const { toggleLoginOpen } = useModalContext();
+
   return (
     <ModalPortal>
-      <Modal>
+      <Modal onClick={toggleLoginOpen}>
         <div className="flex justify-center items-center grow">
           <GoogleLoginButton />
         </div>
